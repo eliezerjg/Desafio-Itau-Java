34162c73e84e2fdf9885acd3e04168e29ade5895
✅ Eliezer Garcia <71297177+eliezerjg@users.noreply.github.com>
2025-03-14 22:49:11 -0300

    fix: alterado dtos para o padrao java bean
    fix: alterado dtos para o padrao java bean

--- src/main/java/br/com/itau/transacoes/infra/rest/dto/TransacaoRequestDTO.java
+++ src/main/java/br/com/itau/transacoes/infra/rest/dto/TransacaoRequestDTO.java
@@ -4,6 +4,22 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
 public class TransacaoRequestDTO {
-    public BigDecimal valor;
-    public OffsetDateTime dataHora;
+    private BigDecimal valor;
+    private OffsetDateTime dataHora;
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(OffsetDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
 }
