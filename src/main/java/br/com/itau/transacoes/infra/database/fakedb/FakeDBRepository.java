4e6c7af76b262b1e6c027a84e7066799aa4b509b
✅ Eliezer Garcia <71297177+eliezerjg@users.noreply.github.com>
2025-03-15 12:53:08 -0300

    fix: adicionado metodo de filtragem pelo segundo inicial
    fix: adicionado metodo de filtragem pelo segundo inicial

--- src/main/java/br/com/itau/transacoes/infra/database/fakedb/FakeDBRepository.java
+++ src/main/java/br/com/itau/transacoes/infra/database/fakedb/FakeDBRepository.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 public interface FakeDBRepository<T> {
+    public List<T> getAllByStartTime(Long inicioFiltroEmSegundos);
     public List<T> getAll();
     public void deleteAll(Type t);
     public <T extends WrapperEntity> T create(T entity);
