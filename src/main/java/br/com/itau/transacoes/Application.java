ff582bcf46b85d1ae10b8355128e1b6394b2a771
✅ Eliezer Garcia <71297177+eliezerjg@users.noreply.github.com>
2025-03-17 10:09:02 -0300

    fix: alterado para commandlinerunner para evitar DRY
    fix: alterado para commandlinerunner para evitar DRY

--- src/main/java/br/com/itau/transacoes/Application.java
+++ src/main/java/br/com/itau/transacoes/Application.java
@@ -1,16 +1,25 @@
 package br.com.itau.transacoes;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Value("${br.com.itau.transacoes.timezone:UTC}")
+    private String timeZone;
 
     public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        TimeZone.setDefault(TimeZone.getTimeZone(this.timeZone));
+        System.out.println("Fuso horário configurado para: " + this.timeZone);
+    }
 }
